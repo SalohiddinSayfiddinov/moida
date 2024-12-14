3281cb26e0094a6c38ce38c693c42185875224d5
Salohiddin Sayfiddinov <salokhiddin.sayfiddinov@mail.com>
2024-12-14 16:57:43 +0500

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:moida/menu/menu.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moida/Details/details.dart';
+import 'package:moida/cart/cart.dart';
+import 'package:moida/cubit/cubit_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'RobotoMono',
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
         ),
         useMaterial3: true,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: HomesScreen(),
+      home: CartPage(),
     );
   }
 }
@@ -71,4 +76,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
